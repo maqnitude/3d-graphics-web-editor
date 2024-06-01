216432092ca1cb8b433352b4c054c6ec8035649a
Mai Anh Quân <108073174+qvan7um@users.noreply.github.com>
2024-06-01 10:44:11 +0700

    refactor: bring all cameras to editor.js

--- js/editor/editor.js
+++ js/editor/editor.js
@@ -26,12 +26,33 @@ class Editor {
     this.history = new History( this );
     this.selector = new Selector( this );
 
+    this.perspectiveCamera = this.createPerspectiveCamera();
+    this.orthographicCamera = this.createOrthographicCamera();
+    this.camera = this.perspectiveCamera; // Defaults to perspective camera
+
+    this.viewportCamera = this.camera;
+    this.viewportCamera.name = "Viewport Camera";
+
     this.scene = new THREE.Scene();
     this.scene.name = "Scene";
 
     this.sceneHelper = new THREE.Scene();
     this.sceneHelper.add( new THREE.HemisphereLight( 0xffffff, 0x888888, 2 ) );
   }
+
+  createPerspectiveCamera() {
+    const camera = new THREE.PerspectiveCamera( 50, 1, 0.1, 1000 );
+
+    camera.position.set( 0, 5, 10 );
+    camera.lookAt( new THREE.Vector3( 0, 0, 0 ) );
+
+    return camera;
+  }
+
+  createOrthographicCamera() {
+
+  }
+
 }
 
 export { Editor };
